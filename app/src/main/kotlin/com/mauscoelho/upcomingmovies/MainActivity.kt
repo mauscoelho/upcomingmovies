79180ba2f3ef44ffdd8b721b02c1a44a91693f70
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2016-11-25 15:11:19 -0200

    :smile: starting project

---
+++ app/src/main/kotlin/com/mauscoelho/upcomingmovies/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.mauscoelho.upcomingmovies
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    @Override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
