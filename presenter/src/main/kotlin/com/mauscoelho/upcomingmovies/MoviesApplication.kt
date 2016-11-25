bd0a5acbd4ef775b1aef8a731bb0b05b78d364e2
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2016-11-25 17:04:24 -0200

    :smile: setup dagger and provide retrofit

---
+++ presenter/src/main/kotlin/com/mauscoelho/upcomingmovies/MoviesApplication.kt
@@ -0,0 +1,17 @@
+package com.mauscoelho.upcomingmovies
+
+import android.app.Application
+import com.mauscoelho.upcomingmovies.dagger.AppComponent
+import com.mauscoelho.upcomingmovies.dagger.DaggerAppComponent
+import com.mauscoelho.upcomingmovies.dagger.MainModule
+
+
+class MoviesApplication : Application() {
+    companion object {
+        @JvmStatic lateinit var appComponent: AppComponent
+    }
+    override fun onCreate() {
+        super.onCreate()
+        appComponent = DaggerAppComponent.builder().mainModule(MainModule(this)).build()
+    }
+}
--- presenter/src/main/kotlin/com/mauscoelho/upcomingmovies/view/MainActivity.kt
+++ presenter/src/main/kotlin/com/mauscoelho/upcomingmovies/view/MainActivity.kt
@@ -1,7 +1,9 @@
-package com.mauscoelho.upcomingmovies
+package com.mauscoelho.upcomingmovies.view
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import com.mauscoelho.upcomingmovies.MoviesApplication
+import com.mauscoelho.upcomingmovies.R
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,5 +11,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        MoviesApplication.appComponent.inject(this)
     }
 }
