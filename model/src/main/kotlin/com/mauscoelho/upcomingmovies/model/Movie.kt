63f0a4d4c18096a05354d63b20bf3effbd8b76f8
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2016-11-27 20:45:55 -0200

    :smile: add model movie

---
+++ model/src/main/kotlin/com/mauscoelho/upcomingmovies/model/Movie.kt
@@ -0,0 +1,10 @@
+package com.mauscoelho.upcomingmovies.model
+
+
+class Movie(
+        val id: Int,
+        val original_title: String,
+        val poster_path: String,
+        val overview: String,
+        val release_date: String)  {
+}
