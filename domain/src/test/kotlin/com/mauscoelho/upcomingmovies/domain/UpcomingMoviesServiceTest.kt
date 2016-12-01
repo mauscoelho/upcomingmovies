a92e2b7242de49c30a510d992b44ee81551f2a1c
Mauricio Coelho <mcimauricio.coelho@gmail.com>
2016-12-01 16:52:52 -0200

    :smile: add test for UpcomingMoviesServiceTest but has a problem with schedulers.io()

---
+++ domain/src/test/kotlin/com/mauscoelho/upcomingmovies/domain/UpcomingMoviesServiceTest.kt
@@ -0,0 +1,63 @@
+package com.mauscoelho.upcomingmovies.domain
+
+import br.ufs.github.rxassertions.RxAssertions
+import com.mauscoelho.upcomingmovies.domain.interactor.UpcomingMoviesServiceImpl
+import com.mauscoelho.upcomingmovies.infrastructure.boundary.GenreRepository
+import com.mauscoelho.upcomingmovies.infrastructure.boundary.SearchRepository
+import com.mauscoelho.upcomingmovies.infrastructure.boundary.UpcomingMoviesRepository
+import com.mauscoelho.upcomingmovies.model.Movie
+import com.mauscoelho.upcomingmovies.model.UpcomingMovies
+import org.jetbrains.spek.api.Spek
+import org.jetbrains.spek.api.dsl.context
+import org.jetbrains.spek.api.dsl.describe
+import org.jetbrains.spek.api.dsl.it
+import org.junit.platform.runner.JUnitPlatform
+import org.junit.runner.RunWith
+import org.mockito.Mockito
+import org.mockito.Mockito.mock
+import rx.Observable
+
+@RunWith(JUnitPlatform::class)
+class UpcomingMoviesServiceTest : Spek({
+
+    val upcomingMoviesRepository = mock(UpcomingMoviesRepository::class.java)
+    val genreRepository = mock(GenreRepository::class.java)
+    val searchRepository = mock(SearchRepository::class.java)
+    val genreService = UpcomingMoviesServiceImpl(upcomingMoviesRepository, genreRepository, searchRepository)
+    val apiKey = "1f54bd990f1cdfb230adb312546d765d"
+    val language = "en-US"
+
+    describe("UpcomingMoviesServiceTest") {
+        context("Get movie detail") {
+            it("should return movie detail") {
+                val movieId = 1
+                val expected = Movie(20, 1, 1, "title", "poster_path", "overview", "release_date", "", "", 0.1, listOf(), listOf())
+                val result = Movie(20, 1, 1, "title", "poster_path", "overview", "release_date", "", "", 0.1, listOf(), listOf())
+
+                Mockito.`when`(upcomingMoviesRepository.getMovie(movieId, apiKey, language)).thenReturn(Observable.just(result))
+
+                RxAssertions.assertThat(genreService.getMovie(movieId, apiKey, language))
+                        .completes()
+                        .withoutErrors()
+                        .expectedSingleValue(expected)
+            }
+            it("should return 3 movies") {
+                val page = 1
+                val expectedMovies =  mutableListOf(
+                        Movie(20, 1, 1, "title", "poster_path", "overview", "release_date", "", "", 0.1, listOf(), listOf()),
+                        Movie(20, 1, 2, "title", "poster_path", "overview", "release_date", "", "", 0.1, listOf(), listOf()),
+                        Movie(20, 1, 3, "title", "poster_path", "overview", "release_date", "", "", 0.1, listOf(), listOf()))
+
+                val result = UpcomingMovies(1, expectedMovies.toTypedArray(), 4, 170)
+
+                Mockito.`when`(upcomingMoviesRepository.getUpcomingMovies(apiKey, language, page)).thenReturn(Observable.just(result))
+
+                RxAssertions.assertThat(genreService.getUpcomingMovies(apiKey, language, page))
+                        .completes()
+                        .withoutErrors()
+                        .emissionsCount(3)
+
+            }
+        }
+    }
+})
